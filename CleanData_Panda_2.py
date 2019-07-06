af138f629ae95e5abec254f773d01561900fc477
njxjtu <njxjtu@gmail.com>
2019-07-06 00:22:17 -0700

    Create CleanData_Panda_2.py

---
+++ CleanData_Panda_2.py
@@ -0,0 +1,14 @@
+'''
+Part 2
+For the next set of questions, we will be using census data from the United States Census Bureau. Counties are political and geographic subdivisions of states in the United States. This dataset contains population data for counties and states in the US from 2010 to 2015. See this document for a description of the variable names.
+
+The census dataset (census.csv) should be loaded as census_df. Answer questions using this as appropriate.
+'''
+
+# Question 5
+# Which state has the most counties in it? (hint: consider the sumlevel key carefully! You'll need this for future questions too...)
+# This function should return a single string value.
+
+import panda as pd
+census_df = pd.read_csv('census.csv')
+census_df.head()

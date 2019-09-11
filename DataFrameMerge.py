f82a14e27b613643bfd0dcca4b8ae8b038d4aff7
njxjtu <njxjtu@gmail.com>
2019-09-11 16:31:59 -0700

    Update DataFrameMerge.py

--- DataFrameMerge.py
+++ DataFrameMerge.py
@@ -1,11 +1,15 @@
+"""
+DataFrame.merge(self, right, how='inner', on=None, left_on=None, right_on=None, left_index=False, right_index=False, sort=False, suffixes=('_x', '_y'), copy=True, indicator=False, validate=None)
+"""
+
 np.random.seed(0)
 left = pd.DataFrame({'key': ['A', 'B', 'C', 'D'], 'value': np.random.randn(4)})    
 right = pd.DataFrame({'key': ['B', 'D', 'E', 'F'], 'value': np.random.randn(4)})
 
 pd.merge(left, right, on='key')
 
-left.merge(right, on='key', how='left')
+pd.merge(left, right, on='key', how='left')
 
-(left.merge(right, on='key', how='left', indicator=True)
+(pd.merge(left, right, on='key', how='left', indicator=True)
      .query('_merge == "left_only"')
      .drop('_merge', 1))

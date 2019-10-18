2cb683c15f8e22fb6699ff1a33ddd1a18dbf553e
njxjtu <njxjtu@gmail.com>
2019-10-18 13:43:04 -0700

    Update Assignment3_MorePandas.py

--- DataScienceInPython/Assignment3_MorePandas.py
+++ DataScienceInPython/Assignment3_MorePandas.py
@@ -3,37 +3,26 @@ Question 1 (20%)
 Load the energy data from the file Energy Indicators.xls, which is a list of indicators of energy supply and renewable electricity production from the United Nations for the year 2013, and should be put into a DataFrame with the variable name of energy.
 
 Keep in mind that this is an Excel file, and not a comma separated values file. Also, make sure to exclude the footer and header information from the datafile. The first two columns are unneccessary, so you should get rid of them, and you should change the column labels so that the columns are:
-
 ['Country', 'Energy Supply', 'Energy Supply per Capita', '% Renewable']
-
 Convert Energy Supply to gigajoules (there are 1,000,000 gigajoules in a petajoule). For all countries which have missing data (e.g. data with "...") make sure this is reflected as np.NaN values.
 
 Rename the following list of countries (for use in later questions):
-
 "Republic of Korea": "South Korea",
 "United States of America": "United States",
 "United Kingdom of Great Britain and Northern Ireland": "United Kingdom",
 "China, Hong Kong Special Administrative Region": "Hong Kong"
 
 There are also several countries with numbers and/or parenthesis in their name. Be sure to remove these,
-
 e.g.
-
 'Bolivia (Plurinational State of)' should be 'Bolivia',
-
 'Switzerland17' should be 'Switzerland'.
-
-
 
 Next, load the GDP data from the file world_bank.csv, which is a csv containing countries' GDP from 1960 to 2015 from World Bank. Call this DataFrame GDP.
 
 Make sure to skip the header, and rename the following list of countries:
-
 "Korea, Rep.": "South Korea", 
 "Iran, Islamic Rep.": "Iran",
 "Hong Kong SAR, China": "Hong Kong"
-
-
 
 Finally, load the Sciamgo Journal and Country Rank data for Energy Engineering and Power Technology from the file scimagojr-3.xlsx, which ranks countries based on their journal contributions in the aforementioned area. Call this DataFrame ScimEn.
 
@@ -57,8 +46,7 @@ def answer_one():
     
     df_obj = energy.select_dtypes(['object'])
     energy.Country = energy.Country.str.replace("\(.*\)","").str.replace('[0-9]','')
-    
-    ##
+
     # GDP = pd.read_csv('world_bank.csv', index_col=0, skiprows=4)
     GDP = pd.read_csv('world_bank.csv', skiprows=4)
     GDP.replace('Korea, Rep.','South Korea', inplace=True)

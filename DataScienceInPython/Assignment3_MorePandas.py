4232432cd25c0264e71832e78ff6ceff4fc37b64
njxjtu <njxjtu@gmail.com>
2019-12-26 15:48:15 -0800

    Update Assignment3_MorePandas.py

--- DataScienceInPython/Assignment3_MorePandas.py
+++ DataScienceInPython/Assignment3_MorePandas.py
@@ -1,26 +1,46 @@
 '''
 Question 1 (20%)
 Load the energy data from the file Energy Indicators.xls, which is a list of indicators of energy supply and renewable electricity production from the United Nations for the year 2013, and should be put into a DataFrame with the variable name of energy.
+
 Keep in mind that this is an Excel file, and not a comma separated values file. Also, make sure to exclude the footer and header information from the datafile. The first two columns are unneccessary, so you should get rid of them, and you should change the column labels so that the columns are:
+
 ['Country', 'Energy Supply', 'Energy Supply per Capita', '% Renewable']
+
 Convert Energy Supply to gigajoules (there are 1,000,000 gigajoules in a petajoule). For all countries which have missing data (e.g. data with "...") make sure this is reflected as np.NaN values.
+
 Rename the following list of countries (for use in later questions):
+
 "Republic of Korea": "South Korea",
 "United States of America": "United States",
 "United Kingdom of Great Britain and Northern Ireland": "United Kingdom",
 "China, Hong Kong Special Administrative Region": "Hong Kong"
+
 There are also several countries with numbers and/or parenthesis in their name. Be sure to remove these,
+
 e.g.
+
 'Bolivia (Plurinational State of)' should be 'Bolivia',
+
 'Switzerland17' should be 'Switzerland'.
+
+
+
 Next, load the GDP data from the file world_bank.csv, which is a csv containing countries' GDP from 1960 to 2015 from World Bank. Call this DataFrame GDP.
+
 Make sure to skip the header, and rename the following list of countries:
+
 "Korea, Rep.": "South Korea", 
 "Iran, Islamic Rep.": "Iran",
 "Hong Kong SAR, China": "Hong Kong"
+
+
+
 Finally, load the Sciamgo Journal and Country Rank data for Energy Engineering and Power Technology from the file scimagojr-3.xlsx, which ranks countries based on their journal contributions in the aforementioned area. Call this DataFrame ScimEn.
+
 Join the three datasets: GDP, Energy, and ScimEn into a new dataset (using the intersection of country names). Use only the last 10 years (2006-2015) of GDP data and only the top 15 countries by Scimagojr 'Rank' (Rank 1 through 15).
+
 The index of this DataFrame should be the name of the country, and the columns should be ['Rank', 'Documents', 'Citable documents', 'Citations', 'Self-citations', 'Citations per document', 'H index', 'Energy Supply', 'Energy Supply per Capita', '% Renewable', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015'].
+
 This function should return a DataFrame with 20 columns and 15 entries.
 '''
 import pandas as pd
@@ -146,3 +166,36 @@ This function should return a single number.
 print("Question 9")
 final_df2['estimatedCDPP'] = final_df2['Citable documents']/final_df2['estimatedPop']
 print(final_df2)
+print(final_df2[['Citations per document','Energy Supply per Capita']].corr(method ='pearson'))
+'''
+Question 10 (6.6%)
+Create a new column with a 1 if the country's % Renewable value is at or above the median for all countries in the top 15, and a 0 if the country's % Renewable value is below the median.
+This function should return a series named HighRenew whose index is the country name sorted in ascending order of rank.
+'''
+print(final_df2.columns)
+median_renewable=final_df2['% Renewable'].median()
+print(median_renewable)
+final_df2['HighRenew'] = np.where(final_df2['% Renewable']>=median_renewable,1,0)
+print(final_df2)
+'''
+Question 11 (6.6%)
+Use the following dictionary to group the Countries by Continent, then create a dateframe that displays the sample size (the number of countries in each continent bin), and the sum, mean, and std deviation for the estimated population of each country.
+
+ContinentDict  = {'China':'Asia', 
+                  'United States':'North America', 
+                  'Japan':'Asia', 
+                  'United Kingdom':'Europe', 
+                  'Russian Federation':'Europe', 
+                  'Canada':'North America', 
+                  'Germany':'Europe', 
+                  'India':'Asia',
+                  'France':'Europe', 
+                  'South Korea':'Asia', 
+                  'Italy':'Europe', 
+                  'Spain':'Europe', 
+                  'Iran':'Asia',
+                  'Australia':'Australia', 
+                  'Brazil':'South America'}
+This function should return a DataFrame with index named Continent ['Asia', 'Australia', 'Europe', 'North America', 'South America'] and columns ['size', 'sum', 'mean', 'std']
+'''
+continentDF = 

752649cb5330db9e94c3f807e00a4054eb1f78d2
njxjtu <njxjtu@gmail.com>
2020-01-06 15:39:17 -0800

    Update Install.py

--- PythonPandasTutorial/Install.py
+++ PythonPandasTutorial/Install.py
@@ -1,5 +1,14 @@
+# Download appropriate installer
+# Set the Python executable file location in PATH
+
 # Install pandas on Windows cmd
 py -m pip install panda
 
 # Install excel support
 py -m pip install xlrd
+
+'''
+Or install the Python/R platform: Anaconda which includes JupyterLab, JupyterNotebook, Spyder, Glueviz, Orange 3, RStudio, VS Code. 
+(I tried to install VS Code alone with Python extensions but the extension doesn't work due to error "Python: SelectInterpreter not found" error. 
+ forums have discussed about this error and determined that the error is due to bug(s) in the extension; Using an older version would solve the error.)
+'''

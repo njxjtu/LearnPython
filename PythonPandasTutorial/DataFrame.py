a7abf19450df98f44507034823d725938734683e
njxjtu <njxjtu@gmail.com>
2020-01-09 14:25:01 -0800

    Update DataFrame.py

--- PythonPandasTutorial/DataFrame.py
+++ PythonPandasTutorial/DataFrame.py
@@ -4,3 +4,32 @@ import pandas as pd
 df = pd.DataFrame()
 print(df)
 
+
+# Create DataFrame - based on Lists
+data = [1,2,3,4,5]
+df = pd.DataFrame(data)
+print(df)
+
+data = [['Alex',10],['Bob',12],['Clarke',13]]
+df = pd.DataFrame(data,columns=['Name','Age'])
+print(df)
+
+df = pd.DataFrame(data,columns=['Name','Age'],dtype=float)
+print(df)
+
+# Create DataFrame - based on Dict of ndarrays / Lists
+data = {'Name':['Tom', 'Jack', 'Steve', 'Ricky'],'Age':[28,34,29,42]}
+df = pd.DataFrame(data)
+print(df)
+
+df = pd.DataFrame(data, index=['rank1','rank2','rank3','rank4'])
+print(df)
+
+# Create DataFrame - based on List of Dicts
+data = [{'a': 1, 'b': 2},{'a': 5, 'b': 10, 'c': 20}]
+df = pd.DataFrame(data)
+
+data = [{'a': 1, 'b': 2},{'a': 5, 'b': 10, 'c': 20}]
+df = pd.DataFrame(data, index=['first', 'second'])
+df = pd.DataFrame(data, index=['first', 'second'], columns=['a', 'b'])
+df = pd.DataFrame(data, index=['first', 'second'], columns=['a', 'b1'])

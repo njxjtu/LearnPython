ff1a4e91f9e102f535cad0681744b00e6035e137
njxjtu <njxjtu@gmail.com>
2020-02-03 14:33:43 -0800

    Update StatisticalFunctions.py

--- PythonPandasTutorial/StatisticalFunctions.py
+++ PythonPandasTutorial/StatisticalFunctions.py
@@ -22,8 +22,11 @@ print(s)
 s['d'] = s['b'] # so there's a tie
 print(s.rank())
 
+# Window Functions
 df = pd.DataFrame(np.random.randn(10, 4),
    index = pd.date_range('1/1/2000', periods=10),
    columns = ['A', 'B', 'C', 'D'])
 print(df)
 print(df.rolling(window=3).mean())
+print(df.expanding(min_periods=3).mean())
+print(df.ewm(com=0.5).mean())

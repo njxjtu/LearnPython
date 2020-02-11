ee1f1ac0758913c932091505ccaaee936a8e0699
njxjtu <njxjtu@gmail.com>
2020-02-11 14:45:03 -0800

    Update CategoricalData.py

--- PythonPandasTutorial/CategoricalData.py
+++ PythonPandasTutorial/CategoricalData.py
@@ -10,3 +10,16 @@ cat=pd.Categorical(['a','b','c','a','b','c','d'], ['c', 'b', 'a'])
 print(cat)
 cat=pd.Categorical(['a','b','c','a','b','c','d'], ['c', 'b', 'a'],ordered=True)
 print(cat)
+
+cat = pd.Categorical(["a", "c", "c", np.nan], categories=["b", "a", "c"])
+df = pd.DataFrame({"cat":cat, "s":["a", "c", "c", np.nan]})
+
+print(df.describe())
+print(df["cat"].describe())
+print(cat.categories)
+print(cat.ordered)
+
+s = pd.Series(["a","b","c","a"], dtype="category")
+s = s.cat.add_categories([4])
+print(s.cat.categories)
+print(s.cat.remove_categories("a"))

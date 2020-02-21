4b0823eeb2f5e8dd0dc1235af16681eff6a39ed7
njxjtu <njxjtu@gmail.com>
2020-02-20 16:44:37 -0800

    Update Assignment4_Project.py

--- DataScienceInPython/Assignment4_Project.py
+++ DataScienceInPython/Assignment4_Project.py
@@ -1,3 +1,4 @@
+'''
 Assignment 4 - Hypothesis Testing
 This assignment requires more individual learning than previous assignments - you are encouraged to check out the pandas documentation to find functions or methods you might not have used yet, or ask questions on Stack Overflow and tag them as pandas and python related. And of course, the discussion forums are open for interaction with your peers and the course staff.
 
@@ -15,3 +16,56 @@ From the Zillow research data site there is housing data for the United States. 
 From the Wikipedia page on college towns is a list of university towns in the United States which has been copy and pasted into the file university_towns.txt.
 From Bureau of Economic Analysis, US Department of Commerce, the GDP over time of the United States in current dollars (use the chained value in 2009 dollars), in quarterly intervals, in the file gdplev.xls. For this assignment, only look at GDP data from the first quarter of 2000 onward.
 Each function in this assignment below is worth 10%, with the exception of run_ttest(), which is worth 50%.
+'''
+import pandas as pd
+import numpy as np
+from scipy.stats import ttest_ind
+
+# Use this dictionary to map state names to two letter acronyms
+states = {'OH': 'Ohio', 'KY': 'Kentucky', 'AS': 'American Samoa', 'NV': 'Nevada', 'WY': 'Wyoming', 'NA': 'National', 'AL': 'Alabama', 'MD': 'Maryland', 'AK': 'Alaska', 'UT': 'Utah', 'OR': 'Oregon', 'MT': 'Montana', 'IL': 'Illinois', 'TN': 'Tennessee', 'DC': 'District of Columbia', 'VT': 'Vermont', 'ID': 'Idaho', 'AR': 'Arkansas', 'ME': 'Maine', 'WA': 'Washington', 'HI': 'Hawaii', 'WI': 'Wisconsin', 'MI': 'Michigan', 'IN': 'Indiana', 'NJ': 'New Jersey', 'AZ': 'Arizona', 'GU': 'Guam', 'MS': 'Mississippi', 'PR': 'Puerto Rico', 'NC': 'North Carolina', 'TX': 'Texas', 'SD': 'South Dakota', 'MP': 'Northern Mariana Islands', 'IA': 'Iowa', 'MO': 'Missouri', 'CT': 'Connecticut', 'WV': 'West Virginia', 'SC': 'South Carolina', 'LA': 'Louisiana', 'KS': 'Kansas', 'NY': 'New York', 'NE': 'Nebraska', 'OK': 'Oklahoma', 'FL': 'Florida', 'CA': 'California', 'CO': 'Colorado', 'PA': 'Pennsylvania', 'DE': 'Delaware', 'NM': 'New Mexico', 'RI': 'Rhode Island', 'MN': 'Minnesota', 'VI': 'Virgin Islands', 'NH': 'New Hampshire', 'MA': 'Massachusetts', 'GA': 'Georgia', 'ND': 'North Dakota', 'VA': 'Virginia'}
+print(states)
+f = open('university_towns.txt', 'r')
+file_contents = f.read()
+print(file_contents)
+f.close()
+
+def get_list_of_university_towns():
+    '''Returns a DataFrame of towns and the states they are in from the 
+    university_towns.txt list. The format of the DataFrame should be:
+    DataFrame( [ ["Michigan", "Ann Arbor"], ["Michigan", "Yipsilanti"] ], 
+    columns=["State", "RegionName"]  )
+    
+    The following cleaning needs to be done:
+
+    1. For "State", removing characters from "[" to the end.
+    2. For "RegionName", when applicable, removing every character from " (" to the end.
+    3. Depending on how you read the data, you may need to remove newline character '\n'. '''
+    df = pd.DataFrame(columns=['State', 'RegionName'])
+    idx = 0
+    with open('university_towns.txt') as file:
+      for line in file:
+            if '[edit]' in line:
+                  tempstate = line.replace('[edit]','')
+                  tempstate = tempstate.replace('\n','')
+            elif ' (' in line:
+                  temptown = line.split(' (')[0]
+                  temptown = temptown.replace('\n','')
+                  df.loc[idx]=[tempstate, temptown]
+                  idx += 1
+    
+    return df
+
+print(get_list_of_university_towns())
+
+def get_recession_start():
+    '''Returns the year and quarter of the recession start time as a 
+    string value in a format such as 2005q3
+
+    NOTE: 
+    A recession is defined as starting with two consecutive quarters of GDP decline, and ending with two consecutive quarters of GDP growth.
+    '''
+    recessq = ""
+    gdp = pd.read_excel('gdplev.xls', header=5, skiprows=2, usecols=[4,5,6])
+    gdp.columns = ['quarter', 'gdp_current', 'gdp_chained']
+    return gdp
+print(get_recession_start())
